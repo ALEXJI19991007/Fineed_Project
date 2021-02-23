9a0ce8ec25c7c017730df9c837aeaf1a41f982bd
ChangMarkusYu <marcus.yu.56@gmail.com>
2021-02-22 20:38:38 -0600

    migrated rss fetching to backend

--- fineed/src/firebase/FirebaseFunction.ts
+++ fineed/src/firebase/FirebaseFunction.ts
@@ -9,3 +9,4 @@ export const newsCrawler = FirebaseFunction('newsCrawler');
 export const updateNewsClick = FirebaseFunction("updateNewsClick");
 export const updateUserHistory = FirebaseFunction("updateUserHistory");
 export const getUserHistory = FirebaseFunction("getUserHistory");
+export const rssFetch = FirebaseFunction("rssFetch");
