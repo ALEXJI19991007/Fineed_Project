a36a1ed2fc27c20949c05b5ab23795fc55db9f9a
ALEXJI19991007 <yu.ji@vanderbilt.edu>
2021-02-22 21:53:10 -0600

    added getUserHistory function

--- fineed/functions/src/index.ts
+++ fineed/functions/src/index.ts
@@ -1,11 +1,7 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 const { parse } = require("node-html-parser");
-// import firebase from "firebase/app";
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+const Parser = require("rss-parser");
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -138,4 +134,46 @@ export const newsCrawler = functions.https.onCall(async (data, _context) => {
   } catch (error) {
     return error;
   }
+});
+
+export const rssFetch = functions.https.onCall(async (data, _context) => {
+  const parser: typeof Parser = new Parser();
+  const contentParser: DOMParser = new DOMParser();
+  const parsedFeed = await parser.parseURL(data.url);
+  //let newsList: Atoms.Item[] = [];
+  let newsList: Object[] = [];
+
+  parsedFeed.items.forEach((item: any) => {
+    // parse the content field
+    let imgURL: string = "";
+    // TODO
+    // This solution is high likely not robust
+    // It seems like the content field generally have a DOM structure as:
+    // <div><img src="/path/to/img"><div>news synopsis</div></div>
+    // Not sure other sources have a simliar strcture.
+    if (item.content) {
+      const contentHTML = contentParser.parseFromString(
+        item.content,
+        "text/html"
+      );
+      const imgTags = contentHTML.getElementsByTagName("img");
+      if (imgTags.length > 0) {
+        imgURL = imgTags[0].src;
+      }
+    }
+
+    newsList.push({
+      companyTag: data.company,
+      sourceTag: data.source,
+      link: item.link,
+      title: item.title,
+      content: item.contentSnippet,
+      imgUrl: imgURL,
+      pubDate: item.pubDate,
+    });
+  });
+  return {
+    title: parsedFeed.description,
+    list: newsList,
+  };
 });
