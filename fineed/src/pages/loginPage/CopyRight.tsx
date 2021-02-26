27bdf56dc6701c35696f0842fd3ff9b3224975ec
Carol Cheng <kerou.cheng@vanderbilt.edu>
2021-02-26 13:20:26 -0600

    login page

--- fineed/src/pages/loginPage/CopyRight.tsx
+++ fineed/src/pages/loginPage/CopyRight.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 
+
 export function CopyRight() {
     return (
-      <Typography variant="body2" color="textSecondary" align="center">
+      <Typography style={{marginTop:'15px'}} variant="body2" color="textSecondary" align="center">
         {'Copyright © '}
         <Link color="inherit" href="https://fineed.io">
           Fineed
