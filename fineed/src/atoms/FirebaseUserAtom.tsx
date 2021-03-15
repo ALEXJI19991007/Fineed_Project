fcafac771c6210c0e72151be47fba78c9e69af16
ALEXJI19991007 <yu.ji@vanderbilt.edu>
2021-03-15 18:51:04 -0500

    preserve userid atom state

--- fineed/src/atoms/FirebaseUserAtom.tsx
+++ fineed/src/atoms/FirebaseUserAtom.tsx
@@ -1,7 +1,11 @@
 import { atom, selector } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 
 export const curUserUidAtom = atom({
     key: 'curUserUidAtom',
     default: "",
+    effects_UNSTABLE: [persistAtom],
   });
