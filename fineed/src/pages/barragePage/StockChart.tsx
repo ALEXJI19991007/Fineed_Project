e5a3ab44dd9ff9a64ea708e50130b69cd7252cf2
872078694 <34040458+872078694@users.noreply.github.com>
2021-03-16 20:13:33 -0500

    barrage 11/n get hover timestamp

--- fineed/src/pages/barragePage/StockChart.tsx
+++ fineed/src/pages/barragePage/StockChart.tsx
@@ -4,43 +4,52 @@ import priceData from './fakeStockData.json';
 
 const ReactHighstock = require('react-highcharts/ReactHighstock')
 
-export function StockChart(){
-    const config = {
-        chart: {
-          type: 'line'
+export function StockChart() {
+  const handleMouseOver = (event: any) => {
+    const curHoverTimeStamp = event.target.x;
+    console.log(`handleMouseOver gets: `, event.target.x);
+  };
+  const config = {
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: 'Apple Inc.'
+    },
+    subtitle: {
+      text: '(NASDAQ: AAPL): 100 days'
+    },
+    plotOptions: {
+      series: {
+        marker: {
+          enabled: false
         },
-        title: {
-          text: 'Apple Inc.'
-        },
-        subtitle: {
-          text: '(NASDAQ: AAPL): 100 days'
-        },
-        plotOptions: {
-          series: {
-            marker: {
-              enabled: false
-            }
+        point: {
+          events: {
+            mouseOver: handleMouseOver
           }
-        },
-        yAxis: {
-          title: {
-            text: 'Price USD',
-          },
-        },
-        xAxis: {
-          type: 'date',
-          labels: {
-            enabled: false
-          },
-        },
-        series: [
-          { 
-            name: 'Price USD',
-            data: priceData
-          }
-        ]
-      };
-    
-    return <div><ReactHighstock config = {config}></ReactHighstock></div>
+        }
+      },
+    },
+    yAxis: {
+      title: {
+        text: 'Price USD',
+      },
+    },
+    xAxis: {
+      type: 'date',
+      labels: {
+        enabled: false
+      },
+    },
+    series: [
+      {
+        name: 'Price USD',
+        data: priceData
+      }
+    ],
+  };
+
+  return <div><ReactHighstock config={config}></ReactHighstock></div>
 }
 
