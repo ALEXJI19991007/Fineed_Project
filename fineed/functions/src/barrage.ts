d587fa3a7f7a86ec92f2e81b73a18a78851a289c
872078694 <34040458+872078694@users.noreply.github.com>
2021-03-24 23:42:40 -0500

    barrage 24/n add clear barrage and fix name issue while deploying functions

--- fineed/functions/src/barrage.ts
+++ fineed/functions/src/barrage.ts
@@ -25,8 +25,7 @@ export const storeUserBarrage = functions.https.onCall(async (data, _context) =>
 
 });
 
-exports.clearBarrage = functions.pubsub.schedule('every 100 minutes').onRun(async (context) => {
-
+exports.clearBarrage = functions.pubsub.schedule('every 1 minutes').onRun(async (context) => {
     const barrageDB = await db.collection('barrage').where('time', '<=', Date.now());
     barrageDB.get().then((querySnapshot) => {
         querySnapshot.forEach(function (doc) {
