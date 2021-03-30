22143bd5a1295c67f452511f21944b2d6a1d6438
ChangMarkusYu <marcus.yu.56@gmail.com>
2021-03-29 20:49:52 -0500

    front end clean up, duplicate check for email/password login, Md5 hash the password

--- fineed/functions/src/CreateUser.ts
+++ fineed/functions/src/CreateUser.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import { db } from "./index";
 import { Response, ERROR } from "./constants";
+import {Md5} from "md5-typescript";
 
 const initialList: string[] = [];
 
@@ -15,18 +16,26 @@ exports.createNewUser_v2 = functions.https.onCall(async (data, _context) => {
       return response;
     }
     // TODO: Need to check whether the email and the username is duplicating (which is not allowed)
-
     const userRef = db.collection("user");
+    const dupEmailSnapshot = await userRef.where("email", "==", data.email).get();
+    const dupIdSnapshot = await userRef.where("id", "==", data.id).get();
+    // if there is email/id duplicate(s)
+    if(!dupEmailSnapshot.empty || !dupIdSnapshot.empty) {
+      response.error = ERROR.PARAM_ERROR;
+      return response;
+    }
+
     // For later register with email
     // const userEntry = (data.id === null || data.id === "") ? userRef.doc() : userRef.doc(data.id);
     const newUserData = {
       id: data.id,
       email: data.email,
       favorite: initialList,
-      first_name: "New",
-      last_name: "User",
+      first_name: "",
+      last_name: "",
       history: initialList,
-      password: "",
+      // md5 hash the password before storing to the database
+      password: Md5.init(data.password),
       username: data.id,
       subscription: initialList,
     };
