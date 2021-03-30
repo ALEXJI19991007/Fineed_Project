f7c6fed2077e40fba9213fd198a433bf0a40c9d2
ALEXJI19991007 <yu.ji@vanderbilt.edu>
2021-03-30 17:26:20 -0500

    refactored: news fiter bar

--- fineed/src/pages/companynewsPage/NewsFilterAndSubscriber.tsx
+++ fineed/src/pages/companynewsPage/NewsFilterAndSubscriber.tsx
@@ -1,6 +1,5 @@
 import { newsListFilterState } from "../../atoms/NewsListFilterAtom";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import Button from "@material-ui/core/Button";
+import { useRecoilState, useRecoilValue } from "recoil";
 import IconButton from "@material-ui/core/IconButton";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import {
@@ -17,17 +16,33 @@ import { userSubscriptionStatusAtom } from "../../atoms/UserSubscriptionStatusAt
 import { curUserUidAtom } from "../../atoms/FirebaseUserAtom";
 import { useEffect } from "react";
 import { ColorType } from "../../selectors/SubscriptionButtonSelector";
+import { makeStyles } from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
+import Chip from "@material-ui/core/Chip";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(0.5),
+    },
+  },
+}));
 
 type NewsFilterAndSubscriberProps = {
   buttonColorStatus: ColorType[];
 };
 
 export function NewsFilterAndSubscriber(props: NewsFilterAndSubscriberProps) {
+  const classes = useStyles();
+
   const [subscriptionStatus, setSubscriptionStatus] = useRecoilState(
     userSubscriptionStatusAtom
   );
   // const setSubscriptionStatus = useSetRecoilState(userSubscriptionStatusAtom);
-  const setFilter = useSetRecoilState(newsListFilterState);
+  const [filter, setFilter] = useRecoilState(newsListFilterState);
   const userId = useRecoilValue(curUserUidAtom);
 
   // We change the state of the atom when Home page is loaded
@@ -84,25 +99,32 @@ export function NewsFilterAndSubscriber(props: NewsFilterAndSubscriberProps) {
     }
   };
 
-  const getColor = (index: number): ColorType => {
+  const getIconColor = (index: number): ColorType => {
     return props.buttonColorStatus[index];
   };
 
+  const getChipColor = (target: string) => {
+    return filter.target === target ? "secondary" : "default";
+  }
+
   const getButtonComponent = (target: string) => {
-    const newsTarget = `news_${target}`;
+    const newsTarget: string = `news_${target}`;
+    const companyShownName: string = COMPANY_COMPANY_SHOWN_NAME_MAP.get(newsTarget) || "C";
     return (
       <>
-        <Button
-          variant="contained"
-          color="primary"
+        <Chip
+          size="medium"
+          avatar={<Avatar>{companyShownName.charAt(0)}</Avatar>}
+          label={companyShownName}
+          clickable
+          color={getChipColor(target)}
           onClick={() => setFilter({ target: target })}
-        >
-          {COMPANY_COMPANY_SHOWN_NAME_MAP.get(newsTarget)}
-        </Button>
-        <IconButton>
+          
+        />
+        <IconButton style={{paddingTop: "6px", marginLeft: "-10px"}}>
           <AddCircleIcon
             fontSize="small"
-            color={getColor(COMPANY_NUMBER_MAP.get(newsTarget) || 0)}
+            color={getIconColor(COMPANY_NUMBER_MAP.get(newsTarget) || 0)}
             onClick={() => {
               subscribeOnClick(target);
             }}
@@ -113,7 +135,7 @@ export function NewsFilterAndSubscriber(props: NewsFilterAndSubscriberProps) {
   };
 
   return (
-    <div>
+    <div className={classes.root}>
       {getButtonComponent("amazon")}
       {getButtonComponent("apple")}
       {getButtonComponent("facebook")}
