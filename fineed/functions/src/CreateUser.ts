102b05162775f9ec711dcb660d512f1fb28f8df3
ChangMarkusYu <marcus.yu.56@gmail.com>
2021-03-30 20:56:47 -0500

    email password login using our own backend implementations

--- fineed/functions/src/CreateUser.ts
+++ fineed/functions/src/CreateUser.ts
@@ -5,30 +5,41 @@ import {Md5} from "md5-typescript";
 
 const initialList: string[] = [];
 
+// For now I am ignoring data.id -- I am assuming that
+// this function is soley used for creating a new user and
+// nothing else.
+// expected data: { email:string, password:string }
 exports.createNewUser_v2 = functions.https.onCall(async (data, _context) => {
   let response: Response = {
     resp: null,
     error: ERROR.NO_ERROR,
   };
   try {
-    if (data.email === null || data.email === "") {
+    if (!data.email) {
       response.error = ERROR.PARAM_ERROR;
       return response;
     }
-    // TODO: Need to check whether the email and the username is duplicating (which is not allowed)
-    const userRef = db.collection("user");
-    const dupEmailSnapshot = await userRef.where("email", "==", data.email).get();
-    const dupIdSnapshot = await userRef.where("id", "==", data.id).get();
-    // if there is email/id duplicate(s)
-    if(!dupEmailSnapshot.empty || !dupIdSnapshot.empty) {
+
+    // TODO: somebody checks if this is right
+    const userCollectionRef = db.collection("user");
+    const dupEmailSnapshot = await userCollectionRef
+      .where("email", "==", data.email)
+      .get();
+    //const dupIdSnapshot = await userRef.where("id", "==", data.id).get();
+    //if(!dupEmailSnapshot.empty || !dupIdSnapshot.empty) {
+
+    // if there is email duplicate(s)
+    if(!dupEmailSnapshot.empty) {
       response.error = ERROR.PARAM_ERROR;
       return response;
     }
 
     // For later register with email
     // const userEntry = (data.id === null || data.id === "") ? userRef.doc() : userRef.doc(data.id);
+    
+    const userDocumentRef = userCollectionRef.doc();
     const newUserData = {
-      id: data.id,
+      id: userDocumentRef.id,
       email: data.email,
       favorite: initialList,
       first_name: "",
@@ -36,12 +47,13 @@ exports.createNewUser_v2 = functions.https.onCall(async (data, _context) => {
       history: initialList,
       // md5 hash the password before storing to the database
       password: Md5.init(data.password),
-      username: data.id,
+      username: userDocumentRef.id,
       subscription: initialList,
     };
-    await userRef.doc(data.id).set(newUserData);
+
+    userDocumentRef.set(newUserData);
     response.resp = {
-        id: newUserData.id,
+        id: userDocumentRef.id,
     }
     return response;
   } catch (error) {
